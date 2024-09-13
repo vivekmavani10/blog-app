19fe24d2290c4e9a01fcaefaf493fcaee34666f7
vivekmavani10 <145802973+vivekmavani10@users.noreply.github.com>
2024-09-13 15:53:29 +0530

    first commit

---
+++ src/components/RTE.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {Editor } from '@tinymce/tinymce-react';
+import {Controller } from 'react-hook-form';
+
+
+export default function RTE({name, control, label, defaultValue =""}) {
+  return (
+    <div className='w-full'> 
+    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+
+    <Controller
+    name={name || "content"}
+    control={control}
+    render={({field: {onChange}}) => (
+      <Editor
+      apiKey='tb9pj9uj81ymyfmvisevlqk2gngw7a8kg7jv9ai7oihm2e6j'
+      initialValue={defaultValue}
+
+      init={{
+        plugins: [
+          // Core editing features
+          'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+          // Your account includes a free trial of TinyMCE premium features
+          // Try the most popular premium features until Sep 27, 2024:
+          'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown',
+        ],
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name',
+        mergetags_list: [
+          { value: 'First.Name', title: 'First Name' },
+          { value: 'Email', title: 'Email' },
+        ],
+        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
+      }}
+      // initialValue="Welcome to TinyMCE!"
+      onEditorChange={onChange}
+    />
+    )}
+    />
+
+     </div>
+  )
+}
